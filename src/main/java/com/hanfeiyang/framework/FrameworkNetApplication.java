c8c7628ef02bd758a1093abe02c930443eb02e18
hfyZone <1178805691@qq.com>
2022-03-01 17:56:43 +0800

    first

---
+++ src/main/java/com/hanfeiyang/framework/FrameworkNetApplication.java
@@ -0,0 +1,13 @@
+package com.hanfeiyang.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrameworkNetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrameworkNetApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanfeiyang/framework/FrameworkNetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanfeiyang.framework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrameworkNetApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
