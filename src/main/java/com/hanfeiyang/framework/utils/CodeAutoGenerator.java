2d65a1942af022868d67c306545ab0a3d2b90759
hanfeiyang <1178805691@qq.com>
2022-03-02 00:00:01 +0800

    2

--- src/main/java/com/hanfeiyang/framework/utils/CodeAutoGenerator.java
+++ src/main/java/com/hanfeiyang/framework/utils/CodeAutoGenerator.java
@@ -2,29 +2,35 @@ package com.hanfeiyang.framework.utils;
 
 
 import com.baomidou.mybatisplus.generator.FastAutoGenerator;
+import com.baomidou.mybatisplus.generator.config.OutputFile;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.Collections;
 
 /**
  * @author HanFeiYang
  * @date 2022/3/1 17:24
  */
 public class CodeAutoGenerator {
-    FastAutoGenerator.create("url", "username", "password")
-            .globalConfig(builder -> {
-        builder.author("baomidou") // 设置作者
-                .enableSwagger() // 开启 swagger 模式
-                .fileOverride() // 覆盖已生成文件
-                .outputDir("D://"); // 指定输出目录
-    })
-            .packageConfig(builder -> {
-        builder.parent("com.baomidou.mybatisplus.samples.generator") // 设置父包名
-                .moduleName("system") // 设置父包模块名
-                .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "D://")); // 设置mapperXml生成路径
-    })
-            .strategyConfig(builder -> {
-        builder.addInclude("t_simple") // 设置需要生成的表名
-                .addTablePrefix("t_", "c_"); // 设置过滤表前缀
-    })
-            .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
-            .execute();
-
+    public static void main(String[] args) {
+        FastAutoGenerator.create("jdbc:mysql://db.youngcloud.top:3306/framework_net",
+                "root",
+                "123456")
+                .globalConfig(builder -> {
+                    builder.author("hanfeiyang") // 设置作者
+                            .enableSwagger() // 开启 swagger 模式
+                            .fileOverride() // 覆盖已生成文件
+                            .outputDir("/Users/hanfeiyang/IdeaProjects/framework_net/src/main/java/com/hanfeiyang/framework/ddns"); // 指定输出目录
+                })
+                .packageConfig(builder -> {
+                    builder.parent("com.hanfeiyang.framework") // 设置父包名
+                            .moduleName("ddns") // 设置父包模块名
+                            .pathInfo(Collections.singletonMap(OutputFile.mapperXml, "D://")); // 设置mapperXml生成路径
+                })
+                .strategyConfig(builder -> {
+                    builder.addInclude("params");// 设置需要生成的表名
+                })
+                .templateEngine(new FreemarkerTemplateEngine()) // 使用Freemarker引擎模板，默认的是Velocity引擎模板
+                .execute();
+    }
 }
